6e64d62303e74481b3d2ea4dc5e2116856f14e56
yusuf075 <yusufkilic190375@gmail.com>
2026-04-21 14:38:39 +0200

    player code

--- the-last-man-/yusuf/Player.cs
+++ the-last-man-/yusuf/Player.cs
@@ -3,8 +3,17 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+
+	[Export] private float _mouseSensitivity = 0.003f;
+
+	[Export] private Camera3D _camera;
+ 	[Export] private float _Speed = 5.0f;
+	[Export] private float _JumpVelocity = 4.5f;
+
+	 public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -19,25 +28,44 @@ public partial class Player : CharacterBody3D
 		// Handle Jump.
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 		{
-			velocity.Y = JumpVelocity;
+			velocity.Y = _JumpVelocity;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_backwards");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
+			velocity.X = direction.X * _Speed;
+			velocity.Z = direction.Z * _Speed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, _Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, _Speed);
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
 	}
+	
+	public override void _Input(InputEvent @event)
+    {
+        // Check of dit een mouse motion event is
+        if (@event is InputEventMouseMotion mouseMotion)
+        {
+               // Horizontal rotation: roteer de HELE speler (Y-axis)
+            this.RotateY(-mouseMotion.Relative.X * _mouseSensitivity);
+
+            // Vertical rotation: roteer alleen de CAMERA (X-axis)
+            _camera.RotateX(-mouseMotion.Relative.Y * _mouseSensitivity);
+
+			// Voorkom dat de camera omdraait (clamp tussen -86° en +86°, of -1.5 en 1.5 radialen)
+            Vector3 cameraRotation = _camera.Rotation;
+            cameraRotation.X = Mathf.Clamp(cameraRotation.X, -1.5f, 1.5f);
+            _camera.Rotation = cameraRotation;
+
+        }
+    }
 }
