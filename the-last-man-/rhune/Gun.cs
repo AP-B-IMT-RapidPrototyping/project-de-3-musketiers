7241c541cc593be56dd4d3b7eaf30229d68a0a66
mmiztery <mmiztery@gmail.com>
2026-04-21 13:31:29 +0200

    Gun V1.

---
+++ the-last-man-/rhune/Gun.cs
@@ -0,0 +1,60 @@
+using Godot;
+
+
+public partial class Gun : Node3D
+{
+    [Export] Marker3D _muzzle;
+    [Export] private Timer _fireRateTimer;
+
+    [Export] private Node3D _weaponMount;
+
+    public override void _Process(double delta)
+    {
+        GlobalTransform = _weaponMount.GlobalTransform;
+    }
+
+    public void OnShoot()
+    {
+        if (!_fireRateTimer.IsStopped())
+            return;
+
+
+        _fireRateTimer.Start();
+
+        Vector3 beamEnd = _muzzle.GlobalPosition - _muzzle.GlobalBasis.Z * 100;
+        ShowBeam(_muzzle.GlobalPosition, beamEnd);
+
+        GD.Print("Railgun Fired!");
+    }
+
+    private void ShowBeam(Vector3 start, Vector3 end)
+    {
+        var beamMesh = new CylinderMesh();
+        beamMesh.TopRadius = 0.02f;
+        beamMesh.BottomRadius = 0.02f;
+        beamMesh.Height = 1.0f;
+
+        var material = new StandardMaterial3D();
+        material.ShadingMode = BaseMaterial3D.ShadingModeEnum.Unshaded;
+        material.AlbedoColor = Colors.Red;
+
+        var beamInstance = new MeshInstance3D
+        {
+            Mesh = beamMesh,
+            MaterialOverride = material,
+            CastShadow = GeometryInstance3D.ShadowCastingSetting.Off
+        };
+
+        GetTree().Root.AddChild(beamInstance);
+
+        Vector3 direction = end - start;
+        float distance = direction.Length();
+
+        beamInstance.GlobalPosition = start + direction / 2;
+        beamInstance.LookAt(end, Vector3.Up);
+        beamInstance.RotateObjectLocal(Vector3.Right, Mathf.Pi / 2);
+        beamInstance.Scale = new Vector3(1, distance, 1);
+
+        GetTree().CreateTimer(0.05).Timeout += () => beamInstance.QueueFree();
+    }
+}
