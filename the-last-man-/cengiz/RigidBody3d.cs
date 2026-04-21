1f4dfe5f2379609c8c36720d73ad39e738cfc5ca
Cengizhan1Gaal <158459440+Cengizhan1Gaal@users.noreply.github.com>
2026-04-21 16:53:28 +0200

    target

---
+++ the-last-man-/cengiz/RigidBody3d.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+public partial class Target : RigidBody3D
+{
+	[Export] private Node3D _root;
+
+	public void OnHit()
+	{
+		GD.Print("Target destroyed!");
+
+		// Verwijder de hele Target scene
+		_root.QueueFree();
+	}
+}
---
+++ the-last-man-/cengiz/Target.cs
@@ -0,0 +1,6 @@
+using Godot;
+using System;
+
+public partial class Target : Node3D
+{
+}
